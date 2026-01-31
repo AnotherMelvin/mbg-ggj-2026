0b128e8ac8d71c59df2791317575463261abdb26
AnotherMelvin <anothermelvinofficial@gmail.com>
2026-01-31 10:19:15 +0700

    Create Audio Manager

--- Scripts/Utility.gd
+++ Scripts/Utility.gd
@@ -1,1 +1,9 @@
 extends Node
+
+enum SOUND_EFFECT_TYPE {
+	BASE
+}
+
+var rng = RandomNumberGenerator.new()
+func get_rng(min_value: float, max_value: float) -> float:
+	return rng.randf_range(min_value, max_value)
