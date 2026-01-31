29651329d68e45929363fa13d70ac7545c9f1ef2
AnotherMelvin <anothermelvinofficial@gmail.com>
2026-01-31 12:00:29 +0700

    Create Scene Manager

--- Scripts/Debug.gd
+++ Scripts/Debug.gd
@@ -3,6 +3,7 @@ extends Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	SceneManager.transition_to(Utility.SCENE_NAME_TYPE.MAIN, Utility.SCENE_TRANSITION_TYPE.FADE)
 	AudioManager.create_audio(Utility.SOUND_EFFECT_TYPE.BASE)
 
 
